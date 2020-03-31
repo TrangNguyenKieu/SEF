e903f5b602633b0e04c33337987dff6075b77f22
Trang <s3360696@rmit.edu.vn>
2020-03-31 11:15:51 +1100

    second commit

--- SEF Real Estate/src/RealEstate.java
+++ SEF Real Estate/src/RealEstate.java
@@ -1,7 +1,117 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RealEstate {
+	
+	Customer currentCustomer; 
+	Employee currentEmployee;
+	
+	private int choice;
+	private Scanner scan = new Scanner(System.in);
+	private boolean quit; // quit the system
+	private boolean quitToStudentMenu;
+	private boolean logOut;
+	private int currentIndex; //need to implement this in login() to locate employee in array
+	
+	ArrayList<Customer> allCustomers= new ArrayList<Customer>();
+	ArrayList<Employee> allEmployees=new ArrayList<Employee>();
+	ArrayList<Property> allProperties=new ArrayList<Property>();
+	
+	public void landingPageMenu() {
+		quit = false;
+		while (!quit) {
 
-	public void firstMenu() {
-		System.out.println("First menu here");
+			System.out.println("*******Real Estate System******");
+			System.out.printf("1. Log in");
+			System.out.println();
+			System.out.printf("2. Register");
+			System.out.println();
+			System.out.printf("3. Quit");
+			System.out.println();
+
+			enterChoice();
+
+			switch (choice) {
+			case 1:
+				login();
+				break;
+			case 2:
+				register();
+				break;
+				
+			case 3:
+				quit();
+				break;
+
+			default:
+				System.out.println("No such operation");
+				break;
+			}
+		}
+	}
+	
+	
+	
+	public void landLordMenu() {
+		System.out.println("Displaying landlord menu");
+	}
+	
+	public void tenantMenu() {
+		System.out.println("Displaying Tenant menu");
+	}
+	
+	public void branchManager() {
+		System.out.println("Displaying branch manager menu");
+	}
+	
+	public void propertyManager() {
+		System.out.println("Displaying branch manager menu");
+	}
+	
+	
+	
+	public void enterChoice() {
+		boolean valid = false;
+		while (!valid) {
+			try {
+				System.out.print("Enter your choice:");
+				choice = Integer.parseInt(scan.nextLine());
+				valid = true;
+			} catch (Exception e) {
+				System.out.println("No valid choce. Re-enter");
+			}
+		}
+	}
+	public void login() {
+		boolean infoOk=false;
+		while(!infoOk) {
+			System.out.println("Login process..."+"\n");
+			System.out.println("size:"+ allCustomers.size());
+			//login process will need to determine from the input ID and password:
+			//-whether current user is a Employee or Customer
+			//-the index of the user in the related array.
+			
+			currentIndex=0; //after login this variable will store the index of current user in the array
+			currentCustomer=allCustomers.get(currentIndex); //currently hard-coded in StartUp.java
+			
+			if(currentCustomer instanceof Landlord) {
+				landLordMenu();
+			} else if (currentCustomer instanceof Tenant) {
+				tenantMenu();
+			}
+			infoOk=true;
+		}
+		
+	}
+	public void register() {
+		System.out.println("Registration process..."+ "\n");
+	}
+	public void quit() {
+		System.out.println("Program stops here.");
+		quit = true;
+	}
+	
+	public ArrayList<Customer> getAllCustomers(){
+		return allCustomers;
 	}
 }
--- SEF Real Estate/src/StartUp.java
+++ SEF Real Estate/src/StartUp.java
@@ -4,7 +4,15 @@ public class StartUp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RealEstate re= new RealEstate();
-		re.firstMenu();
+		Landlord landlord= new Landlord();
+		Tenant tenant=new Tenant();
+		re.getAllCustomers().add(landlord);
+		re.getAllCustomers().add(tenant);
+		
+		re.landingPageMenu();
+		
+		System.out.println(re.getAllCustomers().size());
+		System.out.println(re.getAllCustomers().get(0));
 	}
 
 }
